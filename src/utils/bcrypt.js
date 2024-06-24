1123cb4a5340753b5cb074416a0e3679d6e65146
enzozal23 <enzo.z.1.ez@gmail.com>
2024-06-24 20:57:40 -0300

    correccion segundo integrador

--- src/utils/bcrypt.js
+++ src/utils/bcrypt.js
@@ -1,4 +1,4 @@
 import bcrypt from 'bcrypt'
 
 export const createHash = password => bcrypt.hashSync(password, bcrypt.genSaltSync(10))
-export const isValidPassword = (password, user) => bcrypt.compareSync(password, user.password)
+export const isValidPassword = (user, password) => bcrypt.compareSync(password, user.password)
